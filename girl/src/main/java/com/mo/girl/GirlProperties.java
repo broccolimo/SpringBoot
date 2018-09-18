43fff543e6e85d7b5cbd628cc04f5505c85578fc
broccolimo <527535602@qq.com>
2018-09-18 10:08:40 +0800

    项目初始化

---
+++ girl/src/main/java/com/mo/girl/GirlProperties.java
@@ -0,0 +1,34 @@
+package com.mo.girl;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author 音神
+ * @date 2018/9/18 9:11
+ */
+
+@Component
+@ConfigurationProperties(prefix = "girl")
+public class GirlProperties {
+
+    private String cupSize;
+
+    private int age;
+
+    public String getCupSize() {
+        return cupSize;
+    }
+
+    public void setCupSize(String cupSize) {
+        this.cupSize = cupSize;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
